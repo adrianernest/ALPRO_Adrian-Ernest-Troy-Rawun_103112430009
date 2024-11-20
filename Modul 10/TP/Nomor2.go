40a6e5b721459c63765d07378d9bc0b72b34088b
adrianernest <162671599+adrianernest@users.noreply.github.com>
2024-11-20 08:32:40 +0700

    Update Nomor2.go

--- Modul 10/TP/Nomor2.go
+++ Modul 10/TP/Nomor2.go
@@ -10,15 +10,16 @@ func main() {
 	fmt.Print("Masukkan usia Anda: ")
 	fmt.Scan(&usia)
 
-	if usia >= 0 && usia <= 12 {
+	switch {
+	case usia >= 0 && usia <= 12:
 		fmt.Println("Kategori: Anak-anak")
-	} else if usia >= 13 && usia <= 17 {
+	case usia >= 13 && usia <= 17:
 		fmt.Println("Kategori: Remaja")
-	} else if usia >= 18 && usia <= 64 {
+	case usia >= 18 && usia <= 64:
 		fmt.Println("Kategori: Dewasa")
-	} else if usia >= 65 {
+	case usia >= 65:
 		fmt.Println("Kategori: Lansia")
-	} else {
+	default:
 		fmt.Println("Usia tidak valid")
 	}
 }
